5bcffa09928f2b10da6e3fedc6860aa92df1d00a
YJLin <0xff07@gmail.com>
2018-10-31 07:50:30 -0700

    Implement isLess

--- bits.c
+++ bits.c
@@ -952,7 +952,13 @@ int isGreater(int x, int y)
  */
 int isLess(int x, int y)
 {
-    return 42;
+    unsigned x_ = y;
+    unsigned y_ = x;
+    unsigned res = x_ + ~y_ + 1;
+    unsigned sgnx = (x_ >> 31) & 1;
+    unsigned sgny = (y_ >> 31) & 1;
+    unsigned sgn = (sgnx ^ 1) & (sgny ^ 0);
+    return (sgn | ((!(sgnx ^ sgny)) & !(res >> 31))) & !!(x ^ y);
 }
 
 /*
